7c79151d3447dff816838c90d245c50b5730a022
HiiWee <wpdlzhf159@gmail.com>
2022-01-30 23:59:49 +0900

    Annotation으로 매핑하기

--- webPrj/src/main/java/com/hoseok/web/controller/IndexController.java
+++ webPrj/src/main/java/com/hoseok/web/controller/IndexController.java
@@ -1,27 +1,30 @@
 package com.hoseok.web.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 // 서블릿을 이용하지 않고 POJO클래스로 컨트롤러를 구성
-public class IndexController implements Controller {
-
-    @Override
-    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        // ViewResolver를 이용해 경로를 간편하게 줄임
-        ModelAndView mv = new ModelAndView("root.index");
-        // data키워드를 가진 String data를 담음
-        mv.addObject("data", "Hello Spring MVC~");
-        // view에 대한 정보를 담음, 두가지 방법이 있다.
-            // 1. view 객체를 따로 만들어서 설정 : mv.setView(View view);
-            // 2. view의 이름만 담아서 전달 : mv.setViewName(@Nullable String viewName);
-        // mv.setViewName("/WEB-INF/view/index.jsp"); 오버로드 생성자 이용
-        
-        return mv;
+@Controller
+public class IndexController {
+    
+    @RequestMapping("/index")
+    public void aaa() {
+        System.out.println("hello");
     }
+    
+
+//    @Override
+//    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+//        // ViewResolver를 이용해 경로를 간편하게 줄임
+//        ModelAndView mv = new ModelAndView("root.index");
+//        // data키워드를 가진 String data를 담음
+//        mv.addObject("data", "Hello Spring MVC~");
+//        // view에 대한 정보를 담음, 두가지 방법이 있다.
+//            // 1. view 객체를 따로 만들어서 설정 : mv.setView(View view);
+//            // 2. view의 이름만 담아서 전달 : mv.setViewName(@Nullable String viewName);
+//        // mv.setViewName("/WEB-INF/view/index.jsp"); 오버로드 생성자 이용
+//        
+//        return mv;
+//    }
 
 }
