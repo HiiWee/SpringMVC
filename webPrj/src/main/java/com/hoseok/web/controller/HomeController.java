402c59f186d21cc675bc940db1550ce7100eff2a
HiiWee <wpdlzhf159@gmail.com>
2022-02-02 03:19:50 +0900

    문서 출력방법 4가지

--- webPrj/src/main/java/com/hoseok/web/controller/HomeController.java
+++ webPrj/src/main/java/com/hoseok/web/controller/HomeController.java
@@ -1,25 +1,48 @@
 package com.hoseok.web.controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 // 서블릿을 이용하지 않고 POJO클래스로 컨트롤러를 구성
 @Controller
 @RequestMapping("/")
 public class HomeController {
+        
+    // 1. 서블릿 객체를 이용해 문자열 출력
+    @RequestMapping("hello1")
+    public void index(HttpServletResponse response) {
+        PrintWriter out;
+        try {
+            out = response.getWriter();
+            out.println("Hello Index");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //return "root.index";
+    }
+    // 2. @ResponseBody 설정을 통한 문자열 출력하기
+    @RequestMapping("hello2")
+    @ResponseBody
+    public String hello() {
+        return "Hello Index!!";
+    }
     
+    // 3. ResourceViewResolver를 이용한 문서 출력하기(index.jsp의 main부분만 출력됨)
     @RequestMapping("index")
-    public String index() {
+    public void index2() {
+    }
+    
+    // 4. TilesViewResolver를 이용한 문서 출력하기
+    @RequestMapping("index2")
+    public String index3() {
         return "root.index";
     }
-
-    @RequestMapping("help")
-    public void help() {
-        System.out.println("help");
-    }
-    
-    
-    
 
 //    @Override
 //    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
