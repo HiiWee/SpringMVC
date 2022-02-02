8fe615203cc8c40056441a1c2d2295123daaaaa2
HiiWee <wpdlzhf159@gmail.com>
2022-02-03 05:22:05 +0900

    JSON 출력하기

--- webPrj/src/main/java/com/hoseok/web/controller/api/NoticeController.java
+++ webPrj/src/main/java/com/hoseok/web/controller/api/NoticeController.java
@@ -1,15 +1,27 @@
 package com.hoseok.web.controller.api;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.hoseok.web.entity.Notice;
+import com.hoseok.web.service.NoticeService;
 
 @RestController("apiNoticeController")
 @RequestMapping("/api/notice/")
 public class NoticeController {
     
+    @Autowired
+    private NoticeService service;
+    
     @RequestMapping("list")
-    public String list() {
+    public List<Notice> list() throws ClassNotFoundException, SQLException {
         
-        return "공지사항 list";
+        List<Notice> list = service.getList(1, "title", "");
+        
+        return list;
     }
 }
