c9ca081516a35e93d25c8361e2d24612fbb4db74
HiiWee <wpdlzhf159@gmail.com>
2022-02-03 20:35:23 +0900

    QueryString 입력 #1

--- webPrj/src/main/java/com/hoseok/web/controller/customer/NoticeController.java
+++ webPrj/src/main/java/com/hoseok/web/controller/customer/NoticeController.java
@@ -3,6 +3,8 @@ package com.hoseok.web.controller.customer;
 import java.sql.SQLException;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,8 +21,10 @@ public class NoticeController {
     private NoticeService noticeService;
     
     @RequestMapping("list")
-    public String list() throws ClassNotFoundException, SQLException {
-        List<Notice> list = noticeService.getList(1, "title", "");
+    public String list(String p /*HttpServletRequest request*/) throws ClassNotFoundException, SQLException {
+        //String p = request.getParameter("p");
+        System.out.println("page : " + p);
+       // List<Notice> list = noticeService.getList(1, "title", "");
         return "notice.list";
     }
 
