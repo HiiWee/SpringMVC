6fc90d0c43c8d1408737ed6bab1b8dc7d7c9ff67
HiiWee <wpdlzhf159@gmail.com>
2022-02-04 19:43:20 +0900

    @RequestParam optional 속성

--- webPrj/src/main/java/com/hoseok/web/controller/customer/NoticeController.java
+++ webPrj/src/main/java/com/hoseok/web/controller/customer/NoticeController.java
@@ -1,16 +1,12 @@
 package com.hoseok.web.controller.customer;
 
 import java.sql.SQLException;
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.hoseok.web.entity.Notice;
 import com.hoseok.web.service.NoticeService;
 
 @Controller
@@ -21,7 +17,7 @@ public class NoticeController {
     private NoticeService noticeService;
 
     @RequestMapping("list")
-    public String list(@RequestParam(name = "p", defaultValue = "1") int page /* HttpServletRequest request */) throws ClassNotFoundException, SQLException {
+    public String list(@RequestParam(value = "p", defaultValue = "1") int page /* HttpServletRequest request */) throws ClassNotFoundException, SQLException {
         //String p = request.getParameter("p");
         System.out.println("page : " + page);
         // List<Notice> list = noticeService.getList(1, "title", "");
